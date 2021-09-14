46b90cf129254553c8159b70d75a60f5fd3d80f9
july22lop <88002008+july22lop@users.noreply.github.com>
2021-09-13 18:15:39 -0600

    codigo

--- EjercicioParcial01/EjercicioParcial01/Entidades/ClsUsuario.cs
+++ EjercicioParcial01/EjercicioParcial01/Entidades/ClsUsuario.cs
@@ -8,9 +8,28 @@ namespace EjercicioParcial01.Entidades
 {
     class ClsUsuario
     {
+        public String Acceso(Usuario usuario)
+        {
+            if (usuario.Nombre == "") 
+            {
+                return "Ingrese aca su nombre: ";
+            
+            
+            }
+            else if (usuario.Contraseña == "") 
+            {
+                return "Ingrese aca su contrasela: ";
+                       
+            }
+            else 
+            {
+                return "Se inicio secion correctamente puede continuar con el proceso";
+            
+            
+            }
 
 
-   
+        }
 
     }
 }
--- EjercicioParcial01/EjercicioParcial01/Entidades/ClsVentas.cs
+++ EjercicioParcial01/EjercicioParcial01/Entidades/ClsVentas.cs
@@ -8,5 +8,37 @@ namespace EjercicioParcial01.Entidades
 {
     class ClsVentas
     {
+        private int identificar_producto;
+        private string nombre_producto;
+        private string descripcion_Producto;
+        private int cantidad;
+        private double precio;
+
+        public ventas(int identificar_producto, string nombre_producto, string descripcion_Producto, int cantidad, double precio)
+        {
+            this.identificar_producto = identificar_producto;
+            this.nombre_producto = nombre_producto;
+            this.descripcion_Producto = descripcion_Producto;
+            this.cantidad = cantidad;
+            this.precio = precio;
+        }
+
+        public ventas(double identificador_producto, string nombre_producto, string descripcion, double precio, int cantidad)
+        {
+            this.nombre_producto = nombre_producto;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+
+        public int Identificar_producto { get => identificar_producto; set => identificar_producto = value; }
+        public string Nombre_producto { get => nombre_producto; set => nombre_producto = value; }
+        public string Descripcion_Producto { get => descripcion_Producto; set => descripcion_Producto = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public double Precio { get => precio; set => precio = value; }
+
+        internal class ventas
+        {
+
+        }
     }
 }
