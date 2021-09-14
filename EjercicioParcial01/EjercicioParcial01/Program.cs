ff91c5471aa385e6cfb57c2af68165027f68e4f7
july22lop <88002008+july22lop@users.noreply.github.com>
2021-09-13 18:08:50 -0600

    Codigo

--- EjercicioParcial01/EjercicioParcial01/Program.cs
+++ EjercicioParcial01/EjercicioParcial01/Program.cs
@@ -10,6 +10,34 @@ namespace EjercicioParcial01
     {
         static void Main(string[] args)
         {
+            String nombre = Console.ReadLine();
+            string contraseña = Convert.ToString(Console.ReadLine());
+
+            Usuario cons = new Usuario(nombre, contraseña);
+            ClsUsuario clsUsuario = new ClsUsuario();
+
+            Console.WriteLine("Digite su nombre de usuario");
+            cons.Nombre = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(clsUsuario.Acceso(Cons));
+
+            if (clsUsuario.Y == 1) 
+            {
+                int x = 1;
+                while (x == 1) 
+                {
+                    Console.WriteLine("Ingrese el ID del producto");
+                    double id_producto = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Ingrese el nombre del producto");
+                    string nombre_producto = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese la descripcion del producto");
+                    string descripcion_producto = Convert.ToString(Console.ReadLine());
+                   
+                
+                }
+            
+            
+            }
         }
     }
 }
